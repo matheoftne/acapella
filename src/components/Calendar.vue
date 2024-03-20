3b0482a4bf1f3f6320de329c2d8c7de1e14b2047
SAMUEL LANDAIS <samuel.landais.pro@gmail.com>
2024-03-20 10:51:25 +0100

    connexion to firebase +  date formatting

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,41 +1,55 @@
 <script setup>
-    import { Calendar, DatePicker } from 'v-calendar';
-    import 'v-calendar/style.css';
-    import { ref } from 'vue';
+import { onUnmounted, ref, onMounted } from 'vue';
+import { db } from '../firebase.js';
+import { collection, query, onSnapshot, doc, deleteDoc, setDoc, addDoc } from 'firebase/firestore';
+import { Calendar, DatePicker } from 'v-calendar';
+import 'v-calendar/style.css';
 
-    const selectedDate = ref(null);
-    const selectedHour = ref(null);
+// Define refs
+const days = ref([]);
+const selectedDate = ref(null);
+const masks = ref({
+  modelValue: 'DD-MM-YYYY HH:mm',
+});
 
-    function updateDateWithHour() {
-    if (selectedHour.value !== null && selectedDate.value) {
-        const dateWithHour = new Date(selectedDate.value);
-        dateWithHour.setHours(selectedHour.value);
-        selectedDate.value = dateWithHour;
-    }
-}
-    
+// Define functions and lifecycle hooks
+onMounted(() => {
+  const q = query(collection(db, 'days'));
+  const unsubscribe = onSnapshot(q, (snapshot) => {
+    days.value = snapshot.docs.map(doc => ({
+      ...doc.data(),
+      id: doc.id
+    }));
+  });
+  
+  onUnmounted(() => {
+    unsubscribe();
+  });
+});
+
 </script>
 
 
+
 <template>
-  <div class="calendar">
-    <DatePicker is-dark expanded trim-weeks v-model="selectedDate">
-        <template #footer>
-            <div class="footer-container" v-if="selectedDate">
-                <form action="" v-on:submit.prevent="console.log('date: ', selectedDate, 'heure: ', selectedHour)">
-                    <select v-model="selectedHour" @change="updateDateWithHour">
-                        <option value="null">Select Hour</option>
-                        <option v-for="hour in 24" :value="hour">{{ hour }}:00</option>
-                    </select>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
-        </template>
-    </DatePicker>
-  </div>
+    <div class="calendar">
+        <DatePicker v-model.string="selectedDate" :masks="masks" mode="dateTime" is-dark is-required trim-weeks />
+        <button @click="console.log(selectedDate ? 'date : ' + selectedDate : 'Date undefined')">Log</button>
+    </div>
+
+    <div class="print-test" v-for="day in days">
+        <p>{{ day }}</p>
+    </div>
+
 </template>
 
 
 <style scoped>
+
+  .calendar {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
 
 </style>
