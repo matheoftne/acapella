7365dd8c388195eecdf2a273f03b8e9d7eaa7ea8
SAMUEL LANDAIS <samuel.landais.pro@gmail.com>
2024-03-21 16:12:39 +0100

    init footer component

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
 import Chat from './components/Chat/Conversation.vue'
 import Header from './components/Header.vue'
 import Quiz from './components/Quiz.vue'
+import Footer from './components/Footer.vue'
 
 </script>
 
 <template>
   <Header />
   <router-view/>
+  <Footer />
 </template>
 
 <style scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<template>
+    <footer class="pad-base">
+        <nav>
+            <router-link to="/">Parlons en ?</router-link>
+            <router-link to="/">Cyberharcèlement</router-link>
+            <router-link to="/">A propos</router-link>
+            <router-link to="/">Blog</router-link>
+            <router-link to="/quiz">Quiz</router-link>
+            <router-link to="/chat-pro">Chat</router-link>
+            <router-link class="btn contactBtn" to="/">Contact</router-link>
+        </nav>
+    </footer>
+
+</template>
+
+<style>
+    
+</style>
