a5187ebfec3b57942a61762a9a92458a5612b735
SAMUEL LANDAIS <samuel.landais.pro@gmail.com>
2024-03-22 09:52:30 +0100

    added team pictures

--- src/components/TeamContainer.vue
+++ src/components/TeamContainer.vue
@@ -6,7 +6,7 @@ export default {
                 {
                     name: 'Théa Gomes',
                     role: 'Coach de vie',
-                    img: 'thea.png'
+                    img: '/thea.png'
                 },
                 {
                     name: 'Tom Raimbault',
@@ -57,7 +57,9 @@ export default {
     <div class="allTeamsContainer" style="display: flex; justify-content: center; align-items: center; flex-direction: column; gap: 4em; margin: 8em">
         <div style="display: flex; justify-content: center; align-items: flex-start; gap: 6em;">
             <div class="teamContainer" v-for="team in team1" style="display: flex; justify-content: center; align-items: flex-start; flex-direction: column; gap: 2em;">
-                <div class="imgContainer" style="background-color: red; width: 10em; height: 10em;"></div>
+                <div class="imgContainer" style="width: 306px; aspect-ratio: 1/1; border-radius: 20px; overflow: clip;">
+                    <img :src="team.img" :alt="team.name" style="width: 100%; aspect-ratio: 1/1; object-fit: cover; object-position: center">
+                </div>
                 <div style="text-align: start;">
                     <p class="name" style="font-weight: 700;">{{ team.name }}</p>
                     <p class="role" style="font-size: 0.7em;">{{ team.role }}</p>
@@ -66,7 +68,9 @@ export default {
         </div>
         <div style="display: flex; justify-content: center; align-items: flex-start; gap: 6em;">
             <div class="teamContainer" v-for="team in team2" style="display: flex; justify-content: center; align-items: flex-start; flex-direction: column; gap: 2em;">
-                <div class="imgContainer" style="background-color: red; width: 10em; height: 10em;"></div>
+                <div class="imgContainer" style="width: 306px; aspect-ratio: 1/1; border-radius: 20px; overflow: clip;">
+                    <img :src="team.img" :alt="team.name" style="width: 100%; aspect-ratio: 1/1; object-fit: cover; object-position: center">
+                </div>
                 <div style="text-align: start;">
                     <p class="name" style="font-weight: 700;">{{ team.name }}</p>
                     <p class="role" style="font-size: 0.7em; width: 12em">{{ team.role }}</p>
